892e0757daa6cd90e200de320bdfab2ac78841c0
Marcos Lopes Juvencio <marcoslopes.cwb@gmail.com>
2020-06-19 17:29:12 -0300

    Projeto Sidebar

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Projeto Sidebar</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/f223593715.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="wrap">
+        <div id="sidebar">
+            <div class="sidebar-content">
+                <h2>Sidebar</h2>
+            </div>
+        </div>
+        <div id="site-content">
+            <div class="navbar-top">
+                <span class="btn" id="open_close"><i class="fas fa-bars"></i></span>
+                <div class="navtop">
+                    <ul>
+                        <li>Home</li>
+                        <li>Contato</li>
+                        <li>Localização</li>
+                    </ul>
+                </div>
+            </div> 
+            <section class="box-1">
+                <h1>Aqui vamos começar com o conteudo do site</h1>
+            </section>
+        </div>
+
+    </div>
+  
+<script src="js/jquery.js"></script>
+<script src="js/actions.js"></script>
+
+
+
+</body>
+</html>
+            
